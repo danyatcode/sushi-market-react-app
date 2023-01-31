9a77db99636920637264880cdc82e3bba269c577
danyatcode <danyatd22@gmail.com>
2023-01-31 06:12:55 +0200

    Files and data commit

---
+++ sushi-app/src/App.js
@@ -0,0 +1,15 @@
+import Data from './Data'
+
+function App() {
+  console.log(Data.sushi)
+  // const allData = Data.sushi.map( item => <img src={item.image}></img>);
+  // const allData = Data.desserts.map( item => <img src={item.image}></img>);
+  // const allData = Data.drinks.map( item => <img src={item.image}></img>);
+  return (
+   <div>
+      {/* {allData} */}
+   </div>
+  );
+}
+
+export default App;
---
+++ sushi-app/src/Data.js
@@ -0,0 +1,114 @@
+
+const Data = {
+    sushi: [
+    {
+        image : `./images/sushi/salmon.jpg`,
+        name  : 'salmon',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 7.99,  
+    },
+    {
+        image : `./images/sushi/tottori.jpg`,
+        name  : 'tottori',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 10.99,  
+    },
+    {
+        image : `./images/sushi/bossanova.jpg`,
+        name  : 'BOSSANOVA ROLL',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 9.99,  
+    },
+    {
+        image : `./images/sushi/canada.jpg`,
+        name  : 'canada',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 9.99,  
+    },
+    {
+        image : `./images/sushi/fudzi.jpg`,
+        name  : 'fudzi',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 7.99,  
+    },
+    {
+        image : `./images/sushi/hayato.jpg`,
+        name  : 'hayato',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 6.99,  
+    },
+    {
+        image : `./images/sushi/hoshi.jpg`,
+        name  : 'hoshi',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 9.99,  
+    },
+    {
+        image : `./images/sushi/kinhe_tobiko.jpg`,
+        name  : 'kinhe tobiko',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 8.99,  
+    },
+    {
+        image : `./images/sushi/kinhe.jpg`,
+        name  : 'kinhe',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+        price : 7.99,  
+    }],
+
+    drinks : [
+        {
+            image : `./images/drinks/coffee.jpg`,
+            name  : 'coffee',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 3.99,  
+        },
+        {
+            image : `./images/drinks/matcha-tea.jpg`,
+            name  : 'matcha-tea',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 3.99,  
+        },
+        {
+            image : `./images/drinks/sake.jpg`,
+            name  : 'sake',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 5.99,  
+        },
+        {
+            image : `./images/drinks/amazake.jpg`,
+            name  : 'amazake',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 6.99,  
+        },
+    ],
+
+    desserts : [
+        {
+            image : `./images/desserts/dorayoki.jpg`,
+            name  : 'dorayoki',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 6.99,  
+        },
+        {
+            image : `./images/desserts/koney-cookies.jpg`,
+            name  : 'honey cookies',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 3.99,  
+        },
+        {
+            image : `./images/desserts/macarons.jpg`,
+            name  : 'macarons',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 5.99,  
+        },
+        {
+            image : `./images/desserts/tofu-pancake.jpg`,
+            name  : 'tofu pancake',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget.',
+            price : 8.99,  
+        },
+    ]
+    }
+
+export default Data
